8c66bc3ad1faecdca290f44e88f45b601926e794
Arthur Detomi <arthurdetomi@gmail.com>
2024-09-06 16:04:58 -0300

    Implementando pesquisa e refatorando código #17

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,12 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view("dashboard", ["ideas" => Idea::orderBy('created_at', 'DESC')->paginate(5)]);
+        $ideas = Idea::orderBy('created_at', 'DESC');
+
+        if (request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
+        return view("dashboard", ["ideas" => $ideas->paginate(5)]);
     }
 }
