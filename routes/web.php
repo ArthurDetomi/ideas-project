79ef25349386bb6700a11b92eef6fd1abfaf440b
Arthur Detomi <arthurdetomi@gmail.com>
2024-09-10 14:09:19 -0300

    Utilizando resources route #24

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 // Teste
 // APP
-Route::get('', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['prefix' => 'ideas/', 'as' => 'ideas.'], function () {
+Route::resource('ideas', IdeaController::class)->except(['index', 'create', 'show'])->middleware('auth');
 
-    //Route::post('', [IdeaController::class, 'store'])->name('store')->withoutMiddleware(['auth']);
+Route::resource('ideas', IdeaController::class)->only(['show']);
 
-    //Route::get('/{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware(['auth']);
-
-    Route::group(['middleware' => ['auth']], function () {
-        //Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-
-        //Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
-
-        //Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
-
-        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
-    });
-});
-
-
-Route::resource('ideas', IdeaController::class);
+Route::resource('ideas.comments', CommentController::class)->only(['store'])->middleware('auth');
