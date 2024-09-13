c2b1d48a72ab8588f051da356c24e2729180a2fc
Arthur Detomi <arthurdetomi@gmail.com>
2024-09-13 13:34:53 -0300

    Creating feed page and refactoring code #34

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FeedController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\IdeaLikeController;
@@ -47,3 +47,5 @@ Route::get('/terms', function () {
 Route::post('ideas/{idea}/like', [IdeaLikeController::class, 'like'])->middleware('auth')->name('ideas.like');
 
 Route::post('ideas/{idea}/unlike', [IdeaLikeController::class, 'unlike'])->middleware('auth')->name('ideas.unlike');
+
+Route::get('/feed', FeedController::class)->middleware('auth')->name('feed');
