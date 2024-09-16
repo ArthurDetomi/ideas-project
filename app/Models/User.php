cc15da658956c067c25eb0cd8100379203aeb3cf
Arthur Detomi <arthurdetomi@gmail.com>
2024-09-16 14:46:25 -0300

    Removing automatic hash from the password
    Optional

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        // 'password' => 'hashed',
     ];
 
     public function ideas()
