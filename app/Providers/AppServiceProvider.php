e8ce391d46d991201ab759a1b9261663264c4777
Arthur Detomi <arthurdetomi@gmail.com>
2024-09-19 16:30:49 -0300

    Adding cache in the Laravel app #46

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
-        $topUsers = User::withCount('ideas')->orderBy('ideas_count', 'DESC')->limit(5)->get();
+        \Debugbar::enable();
+
+        $topUsers = Cache::remember('topUsers', 60 * 2, function () {
+            return User::withCount('ideas')->orderBy('ideas_count', 'DESC')->limit(5)->get();
+        });
 
         view()->share('topUsers', $topUsers);
     }
